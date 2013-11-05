cea0a32e85372ba40fc1283080abd205a1849439
mwacc <kudryavtsev.konstantin@gmail.com>
2013-11-05 23:07:30 +0200

    update logic to control counters

--- src/redis_get.lua
+++ src/redis_get.lua
@@ -1,19 +1,21 @@
+local parser = require "redis.parser"
+
 local ad_id = ngx.var.id
 
-
-local res = ngx.location.capture("/backend/redis/get",
-    { args = { key = ad_id } }
+local res = ngx.location.capture("/backend/redis/eval",
+    { args = { cmd = 
+    	"if tonumber(redis.call('get',KEYS[1]..'k')) > 0 then local x=redis.call('get',KEYS[1]);"..
+    	"redis.call('DECR',KEYS[1]..'k');return x; else return nil end", 
+    	arg1="1", arg2=ad_id} }
+--     "redis.call('DECR',KEYS[1]..'k'); return 'test'", arg1="1", arg2=ad_id} }
 )
 
-
+io.stderr:write(res.body)
 if res.status ~= 200 then
     ngx.exit(res.status)
 elseif res.body == "$-1\r\n" then
     ngx.exit(404)
 else
-    local s, e = string.find(res.body, "\r\n", 1, true)
-    local length = tonumber(string.sub(res.body, 2, s))
-    local body = string.sub(res.body, e + 1, e + 1 + length)
-    ngx.say(body)
+    ngx.say( tostring(parser.parse_reply( tostring(res.body) )) )
 end 
 
